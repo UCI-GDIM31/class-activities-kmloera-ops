c9a415d5f16e1faf6e3ad16d765b8aa41758c2d4
kmloera-ops <kmloera@uci.edu>
2025-11-03 21:07:28 -0800

    Finished Game

--- In Class Activities/Assets/W6/Scripts/BatManager.cs
+++ In Class Activities/Assets/W6/Scripts/BatManager.cs
@@ -12,13 +12,15 @@ public class BatManager : MonoBehaviour
     // STEP 1 -----------------------------------------------------------------
     // Add a member variable named "_bats" that's an array of BatW6 Components.
     // In the Inspector, add ALL of the bats in the Scene.
-    
+    [SerializeField] private BatW6[] _bats;
+
     // STEP 1 -----------------------------------------------------------------
 
     // STEP 3 -----------------------------------------------------------------
     // Add a member variable named "_messages" that's an array of strings.
     // In the Inspector, add at least a few different messages for the bats to
     //      say when they reach the player.
+    [SerializeField] private string[] _messages;
     
     // STEP 3 -----------------------------------------------------------------
 
@@ -38,7 +40,7 @@ public class BatManager : MonoBehaviour
         // That means the bat at _bats[0] has a timer at _newTextTimers[0],
         //      the bat at _bats[1] has a timer at _newTextTimers[1],
         //      and so on.
-        // _newTextTimers = new [_bats.Length];
+        _newTextTimers = new float[_bats.Length];
         // STEP 6 -------------------------------------------------------------
     }
 
@@ -48,8 +50,11 @@ public class BatManager : MonoBehaviour
         // STEP 7 -------------------------------------------------------------
         // Loop through all of the entries in _newTextTimers, and increase each
         //      timer's value by the amount of time that passed this frame.
-        
 
+        for (int i = 0; i < _newTextTimers.Length; i++)
+        {
+            _newTextTimers[i] += Time.deltaTime;
+        }
         // STEP 7 -------------------------------------------------------------
 
         // STEP 2 -------------------------------------------------------------
@@ -63,7 +68,23 @@ public class BatManager : MonoBehaviour
         // You will need to check the Vector3 documentation to find a method
         //      to help you with that distance check :)
         // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.html
-        //
+        for (int i = 0; i < _bats.Length; i++)
+        {
+            
+            if (Vector3.Distance(_bats[i].transform.position, _playerTransform.position) < _interactDistance)
+            {
+                _bats[i].Bat_start(_playerTransform);
+                if (Vector3.Distance(_bats[i].transform.position, _playerTransform.position) < _overlapDistance)
+                {
+                    CreateReactions(_bats[i]);
+                }
+            }
+            else
+            {
+                _bats[i].Bat_stop();
+            }
+            
+        }
         // STEP 4
         // Also inside this for loop, if the distance between the bat and the
         //      player is less than _overlapDistance, call CreateReactions()
@@ -92,7 +113,9 @@ public class BatManager : MonoBehaviour
         //
         // The first argument to SpawnReactionUI is same bat in the parameters
         //      of CreateReactions.
-        
+        int index = Random.Range(0, _messages.Length);
+
+        SpawnReactionUI(bat, _messages[index]);
         // STEP 5 -------------------------------------------------------------
     }
 
@@ -107,7 +130,7 @@ public class BatManager : MonoBehaviour
         // /* starts the comments, and */ ends it.
         // Simply uncomment the below lines by removing the /* and */ to finish.
 
-        /*
+        
         int index = System.Array.IndexOf(_bats, bat);
         
         GridLayoutGroup layout = bat.GetComponentInChildren<GridLayoutGroup>();
@@ -117,7 +140,7 @@ public class BatManager : MonoBehaviour
             TMP_Text textObj = Instantiate(_reactionUiPrefab, layout.transform);
             textObj.text = message;
         }
-        */
+        
 
         // STEP 8 -------------------------------------------------------------
     }
--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -3,5 +3,37 @@ using UnityEngine;
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
+
+    private Transform target;
+    [SerializeField] private float speed = 1f;
+
+    // public void Start()
+    // {
+    //     Bat_start();
+
+
+    // }
+
+    public void Update()
+    {
+        // this will check whether the bat has performed a certain action and then it will stop the bats movement
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+    }
     
+    public void Bat_start(Transform player)
+    {
+        //bat starts moving here
+        enabled = true;
+        target = player;
+    }
+    
+    public void Bat_stop()
+    {
+        //bat stops moving here when it has collided with the character
+        enabled = false;    
+    }
 }
